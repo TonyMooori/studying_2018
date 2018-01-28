efad02f05ab30b03ffb47fa82e3489f6d5c60e49
TonyMooori <tony.mooori@gmail.com>
2018-01-28 22:12:11 +0900

    play with code

---
+++ 201801/riceball/main.c
@@ -0,0 +1,3 @@
+f(n){for(;n;putchar(n%2?'*':' '),n/=2);putchar('\n');}main(i){int a[7]={0x1CCE76,0x93299,0x93299,0x9EE79,0x92219,0x1D2216,0};for(i=0;a[i];f(a[i++]));}
+// https://ideone.com/xN1V4d
+ 
