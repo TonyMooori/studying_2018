ad5afbee83e980223b9ae0ed247e9d25cd1d92c6
TonyMooori <tony.mooori@gmail.com>
2018-04-03 08:50:09 +0900

    updated define_opcode.v

---
+++ 201803/verilog014/define_opcode.v
@@ -0,0 +1,13 @@
+
+`define OP_MOV_A_IM 4'b0011
+`define OP_MOV_B_IM 4'b0111
+`define OP_MOV_A_B  4'b0001
+`define OP_MOV_B_A  4'b0100
+`define OP_ADD_A_IM 4'b0000
+`define OP_ADD_B_IM 4'b0101
+`define OP_IN_A     4'b0010
+`define OP_IN_B     4'b0110
+`define OP_OUT_IM   4'b1011
+`define OP_OUT_B    4'b1001
+`define OP_JMP_IM   4'b1111
+`define OP_JNC_IM   4'b1110 // jump if not carry
